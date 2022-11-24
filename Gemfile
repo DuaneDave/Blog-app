aeb35bdc8f53a56df3e1c94b44314c1e0fde4458
Duane David <duanedave17@gmail.com>
2022-11-24 07:40:52 -0800

    Resolve linter error

--- Gemfile
+++ Gemfile
@@ -66,7 +66,7 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
-  gem 'rspec-rails', ">= 3.9.0"
+  gem 'rspec-rails', '>= 3.9.0'
 end
 
 group :test do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -78,21 +78,20 @@ RSpec.configure do |config|
 # config.default_formatter = "doc"
 
 
-  # Print the 10 slowest examples and example groups at the
-  # end of the spec run, to help surface which specs are running
-  # particularly slow.
-  config.profile_examples = 10
+# Print the 10 slowest examples and example groups at the
+# end of the spec run, to help surface which specs are running
+# particularly slow.
+# config.profile_examples = 10
 
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = :random
+# Run specs in random order to surface order dependencies. If you find an
+# order dependency and want to debug it, you can fix the order by providing
+# the seed, which is printed after each run.
+#     --seed 1234
+# config.order = :random
 
-  # Seed global randomization in this process using the `--seed` CLI option.
-  # Setting this allows you to use `--seed` to deterministically reproduce
-  # test failures related to randomization by passing the same `--seed` value
-  # as the one that triggered the failure.
-  Kernel.srand config.seed
-=end
+# Seed global randomization in this process using the `--seed` CLI option.
+# Setting this allows you to use `--seed` to deterministically reproduce
+# test failures related to randomization by passing the same `--seed` value
+# as the one that triggered the failure.
+# Kernel.srand config.seed
 end
