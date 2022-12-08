063234b0071b04a47581d66723234d5a3abb3380
Duane David <duanedave17@gmail.com>
2022-12-08 01:40:33 -0800

    Resolve linter error

--- db/migrate/20221207223345_add_confirmable_to_devise_v1.rb
+++ db/migrate/20221207223345_add_confirmable_to_devise_v1.rb
@@ -7,6 +7,6 @@ class AddConfirmableToDeviseV1 < ActiveRecord::Migration[7.0]
       t.datetime :confirmation_sent_at
       t.string   :unconfirmed_email # Only if using reconfirmable
     end
-    add_index  :users, :confirmation_token, :unique => true
+    add_index :users, :confirmation_token, :unique => true
   end
 end
