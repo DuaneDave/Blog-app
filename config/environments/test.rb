62a93dc7ac5d573493d1cf4924db882605f718a6
Duane David <duanedave17@gmail.com>
2022-12-08 01:20:42 -0800

    Modify config for mailer

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,6 +9,19 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      'duanedave19@gmail.com',
+    password:       'french2dave',
+    domain:         'localhost:3000',
+    address:       'smtp.gmail.com',
+    port:          '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
   config.cache_classes = true
