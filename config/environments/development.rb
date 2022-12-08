a9daf13a2e5237dd06f3d645b7ba3beb4eb945df
Duane David <duanedave17@gmail.com>
2022-12-08 01:31:18 -0800

    Add leter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,17 +14,14 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name:      'duanedave19@gmail.com',
-    password:       'french2dave',
-    domain:         'localhost:3000',
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :letter_opener
+
+  config.action_mailer.perform_deliveries = true
+
+
 
   # Settings specified here will take precedence over those in config/application.rb.
 
